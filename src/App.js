ee482868daf54a31ab92caa38c988df5fefb5143
Sarthak Malik <sarthakmalik0810@gmail.com>
2021-06-11 01:12:02 +0530

    added routes, nested routes, navlink

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import Welcome from './pages/Welcome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route exact path="/products" >
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
 
 export default App;
+
+/** our-domain.com/welcome => Welcome Component */
+/** our-domain.com/products */
+/** our-domain.com/productDetail/product-id => ProductDetail.js */
